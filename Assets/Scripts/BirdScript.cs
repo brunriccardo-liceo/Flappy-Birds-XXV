fb20b4bf2e85612c1856af390a659c1cdaa3ac30
RICCARDO <BRUN.RICCARDO@liceoxxv.edu.it>
2026-01-30 15:16:34 +0100

    flappy birds basico, con uccellino e tubi che si muovono

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
 
-    public Rigidbody2D myRigidBody;        
+    public Rigidbody2D myRigidBody; 
+    public float flapStrenght = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameObject.name = "Calogero";
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidBody.linearVelocity = new Vector2(0, 1)*flapStrenght;
+        }
     }
 }
