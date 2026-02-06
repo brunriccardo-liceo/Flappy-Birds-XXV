28464d11bdea5355eeabffd0d2eabdf3148202f6
RICCARDO <BRUN.RICCARDO@liceoxxv.edu.it>
2026-02-06 14:32:19 +0100

    punteggio

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public class logicScript : MonoBehaviour
 {
-    public int playerScore;
+    public int playerScore = 0;
     public TextMeshProUGUI scoreText;
 
     
-
+public void AddScore()
+    {
+        playerScore += 1;
+        scoreText.text = playerScore.ToString();
+    }
 
 
 }
